b5e43f89d8f5353fbefe5bec11d330071f8c79ce
Dacamo <castrom1998@gmail.com>
2020-03-29 17:36:58 -0500

    Adding Main project

---
+++ ExpresionesLogicas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ExpresionesLogicas
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            
+            //agregar las validaciones
+
+            Dictionary<string, List<string>> diccionarioOrigen = new Dictionary<string, List<string>>();
+            string expresion = "((((p=q)|r)&r))";
+            var caracteres = Utilidades.ReconocerCaracteres(expresion);
+            diccionarioOrigen = Utilidades.ReconocerProposiciones(expresion);
+            diccionarioOrigen= Utilidades.RecorrerExpresion(caracteres, expresion, diccionarioOrigen);
+            Utilidades.ImprimirDiccionario(diccionarioOrigen);
+
+
+        }
+    }
+}
